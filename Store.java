5c0b0f1cef274c421deea2490d84de17e898f5be
B V Ajay Kumar <101974908+bv-ajay27@users.noreply.github.com>
2022-07-03 19:53:38 +0530

    akignment changes

--- Store.java
+++ Store.java
@@ -1,7 +1,5 @@
-package PasswordEncryptionDecryption;
-import java.util.HashMap;
-import java.util.Random;
-import java.util.Scanner;
+package passwordEncoding_JAVA_Project;
+import java.util.*;
 
 public class Store {
 		// TODO Auto-generated method stub
@@ -12,28 +10,21 @@ public class Store {
 		System.out.println("2.GET PASSWORD OF ACCOUNT");
 		System.out.println("3.EXIT");
 		int k = sc.nextInt();
-		do {
-		switch(k) 
-        {
-            case 1:
-                storeFun();
-                System.out.println("1.SAVE NEW CREDINALS");
-                System.out.println("2.GET PASSWORD OF ACCOUNT");
-                System.out.println("3.EXIT");
-                k = sc.nextInt();
-                break;
-            case 2:
-                getThePassCode();
-                System.out.println("1.SAVE NEW CREDINALS");
-                System.out.println("2.GET PASSWORD OF ACCOUNT");
-                System.out.println("3.EXIT");
-                k = sc.nextInt();
-                break;
-            }
+		do 
+		{
+			switch(k) 
+			{
+				case 1:
+					storeFun();
+					break;
+				case 2:
+					getThePassCode();
+					break;
+			}
 		}
-        while(k!=3);
+        while(k > 2);
 	}
-	static void getThePassCode() {
+	public static void getThePassCode() {
 		// TODO Auto-generated method stub
 		System.out.println("Enter the ACCOUNT NAME");
 		String accountId = sc.next();
@@ -55,41 +46,41 @@ public class Store {
 		String passCode = "";
 		int userWish = sc.nextInt();
 		if(userWish == 1) 
-        {
-			System.out.print("ENTER PASSWORD");
+        	{
+			System.out.println("ENTER PASSWORD");
 			passCode = sc.next();
 		}
 		else if(userWish == 2)
-        {
+        	{
 			passCode = generatePassword();
-			
+			System.out.println(passCode);
 		}
 		storeTheCredinals(account,passCode);
 		return;
-		
 	}
-		static void storeTheCredinals(String account, String passCode) {
+	static void storeTheCredinals(String account, String passCode) {
+	
+		map.put(account, passCode);
+		System.out.println("Credentials saved Successfully");
+		return;
+	}
 		
-			map.put(account, passCode);
-			System.out.println("Credinals saved Successfully");
-			return;
+	public String generatePassword() {
+		// TODO Auto-generated method stub
+		System.out.println("ENTER THE REQUIRED PASSWORD LENGTH");
+		int len = sc.nextInt();
+		String capitalChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+		String smallChar = "abcdefghijklmnopqrstuvwxyz";
+		String numbers = "0123456789";
+		String Symbols = "!@#$&%";
+		String values = capitalChar+smallChar+numbers+Symbols;
+		Random rndm = new Random();
+		String password = "";
+		for(int i=0;i<len;i++) {
+			password += values.charAt(rndm.nextInt(values.length()));
+			
 		}
-		
-		public String generatePassword() {
-			// TODO Auto-generated method stub
-			System.out.println("ENTER THE REQUIRED PASSWORD LENGTH");
-			int len = sc.nextInt();
-			String capitalChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
-			String smallChar = "abcdefghijklmnopqrstuvwxyz";
-			String numbers = "0123456789";
-			String Symbols = "!@#$%^&*<>?/+_-";
-			String values = capitalChar+smallChar+numbers+Symbols;
-			Random rndm = new Random();
-			String password = "";
-			for(int i=0;i<len;i++) {
-				password += values.charAt(rndm.nextInt(values.length()));
-				
-			}
-			return password;
-        }
-    }
+		return password;
+	}
+}
+
