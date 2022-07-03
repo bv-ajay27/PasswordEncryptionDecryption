4b419c721a1014a6a4367b6314485d64a38d8d33
B V Ajay Kumar <101974908+bv-ajay27@users.noreply.github.com>
2022-07-03 19:50:19 +0530

    alignment changes

--- mainPage.java
+++ mainPage.java
@@ -1,15 +1,17 @@
-package PasswordEncryptionDecryption;
+package passwordEncoding_JAVA_Project;
 import java.util.Scanner;
 
 public class mainPage {
     public static void main(String[] args) {
+        System.out.println();
         System.out.println("WELCOME TO PASSWORD MANAGEMENT SYSTEM");
 		goToInstructions();     
     }
     static void goToInstructions() {
-        //USER user = new USER();
         Store st = new Store();
+// 	USER user = new USER();
         Scanner sc = new Scanner(System.in);
+        System.out.println();
         System.out.println("Enter any number to continue");
         int k = sc.nextInt();
         
@@ -19,73 +21,73 @@ public class mainPage {
             System.out.println("3.Encrypt your password");
             System.out.println("4.Decrypt your password");
             System.out.println("5.Reset your password");
-            System.out.println("9.Exit");
+            System.out.println("0.Exit");
             k = sc.nextInt();
-            switch(k) {
-            case 1:
-                if(USER.userExist() == true) {
-                    System.out.println("User already exist");
+            switch(k) 
+            {
+                case 1:
+                    if(USER.userExist() == true) {
+                        System.out.println("User already exist");
+                        break;
+                    }
+                    System.out.println("Enter Numerical UserID");
+                    int id = sc.nextInt();
+                    System.out.println("1.CHOOSE OWN PASSWORD");
+                    System.out.println("2.GENERATE PASSWORD AUTOMATICALLY");
+                    String password;
+                    int wish = sc.nextInt();
+                    if(wish == 1) {
+                        System.out.println("ENTER PASSWORD");
+                        password = sc.next();
+                    }
+                    else if(wish == 2){
+                        password = st.generatePassword();
+                        System.out.println(password);
+                    }
+                    else {
+                        System.out.println("Invalid Input :(");
+                        break;
+                    }
+                    USER.addUSER(id,password);
                     break;
-                }
-                System.out.println("Enter UserID");
-                int id = sc.nextInt();
-                System.out.println("1.CHOOSE OWN PASSWORD");
-                System.out.println("2.GENERATE PASSWORD AUTOMATICALLY");
-                String password;
-                int wish = sc.nextInt();
-                if(wish == 1) {
-                    System.out.print("ENTER PASSWORD");
-                    password = sc.next();
-                }
-                else if(wish == 2){
-                    password = st.generatePassword();
-                    
-                }
-                else {
-                    System.out.println("Invalid Input :(");
+                case 2:
+                    if(USER.userExist()==false) {
+                        System.out.println("ACCOUNT NOT CREATED YET");
+                        break;
+                    }
+                    System.out.println("ENTER YOUR PASSWORD");
+                    String inputPassword = sc.next();
+                    if(USER.access(inputPassword) == true) {
+                        st.choose();
+                    }
+                    else {
+                        System.out.println("PASSWORD IS INCORRECT");
+                    }
                     break;
-                }
-                USER.addUSER(id,password);
-                break;
-            case 2:
-                if(USER.userExist()==false) {
-                    System.out.println("NO ACCOUNT CREATED YET");
+                case 3:
+                    System.out.println("Enter the passwrd to encode");
+                    String passwordToBeEncrypt = sc.next();
+                    USER.encryptPassword(passwordToBeEncrypt);
                     break;
-                }
-                System.out.println("ENTER YOUR PASSWORD");
-                String inputPassword = sc.next();
-                if(USER.access(inputPassword) == true) {
-                    st.choose();
+                case 4:
+                    System.out.println("Enter the encodedpasswrd to decode");
+                    String encodedPassword = sc.next();
+                    USER.decryptPassword(encodedPassword);
+                    break;
+                case 5:
+                    if(USER.userExist()==false) {
+                        System.out.println("NO ACCOUNT CREATED YET");
+                        break;
+                    }
+                    System.out.println("Enter your prevPassword");
+                    String prevPassword = sc.next();
+                    System.out.println("Enter your newPassword");
+                    String newPassword = sc.next();
+                    USER.setPassword(prevPassword, newPassword);
+                    break;
+                case 0:
+                    break;
             }
-                else {
-                    System.out.println("PASSWORD IS INCORRECT");
-                }
-                break;
-            case 3:
-                System.out.println("Enter the passwrd to encode");
-                String passwordToBeEncrypt = sc.next();
-                USER.encryptPassword(passwordToBeEncrypt);
-                break;
-            case 4:
-                System.out.println("Enter the encodedpasswrd to decode");
-                String encodedPassword = sc.next();
-                USER.decryptPassword(encodedPassword);
-                break;
-            case 5:
-                if(USER.userExist()==false) {
-                    System.out.println("NO ACCOUNT CREATED YET");
-                    break;
-                }
-                System.out.println("Enter your prevPassword");
-                String prevPassword = sc.next();
-                System.out.println("Enter your newPassword");
-                String newPassword = sc.next();
-                USER.setPassword(prevPassword, newPassword);
-                break;
-            case 9:
-                break;
-                
-            }
-        }while(k!=9);
+        }while(k != 0);
     }
 }
